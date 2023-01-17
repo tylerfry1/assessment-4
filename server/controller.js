f127d42dd8b02be9102c9782cd55882d1a2112fe
Tyler Fry <tyler.fry1@outlook.com>
2023-01-16 18:13:51 -0700

    revised commit

--- server/controller.js
+++ server/controller.js
@@ -11,3 +11,14 @@ module.exports = {
     }
 
 }
+
+module.exports ={
+    getFortune: (req, res) => {
+        const fortunes =["All will go well with your new project","A inch of time is an inch of gold","Any day above ground is a good day","All your hard work will soon pay off","Any decision you have to make tomorrow is a good decision"];
+
+        let fortuneIndex = Math.floor(math.random() * fortunes.length);
+        let randomFortune = fortunes[fortuneIndex];
+
+        res.status(200).send(randomFortune);
+    }
+}
