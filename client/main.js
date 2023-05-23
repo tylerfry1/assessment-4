cabc6cab1d52223e1d309408cba9a66c0459bb1a
tylerfry1 <tyler.fry1@outlook.com>
2023-05-22 21:07:46 -0600

    Commit

--- client/main.js
+++ client/main.js
@@ -1,11 +1,21 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
 
 const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+  axios.get("http://localhost:4000/api/compliment/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
 };
 
-complimentBtn.addEventListener('click', getCompliment)
+complimentBtn.addEventListener("click", getCompliment);
+
+const FortuneBtn = document.getElementById("fortuneButton");
+
+const getFortune = () => {
+  axios.get("http://localhost:4000/api/compliment/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
+
+FortuneBtn.addEventListener("click", getFortune);
--- server/controller.js
+++ server/controller.js
@@ -1,24 +1,32 @@
 module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
 
-}
+    res.status(200).send(randomCompliment);
+  },
+};
 
-module.exports ={
-    getFortune: (req, res) => {
-        const fortunes =["All will go well with your new project","A inch of time is an inch of gold","Any day above ground is a good day","All your hard work will soon pay off","Any decision you have to make tomorrow is a good decision"];
+module.exports = {
+  getFortune: (req, res) => {
+    const fortunes = [
+      "All will go well with your new project",
+      "A inch of time is an inch of gold",
+      "Any day above ground is a good day",
+      "All your hard work will soon pay off",
+      "Any decision you have to make tomorrow is a good decision",
+    ];
 
-        let fortuneIndex = Math.floor(math.random() * fortunes.length);
-        let randomFortune = fortunes[fortuneIndex];
+    let fortuneIndex = Math.floor(math.random() * fortunes.length);
+    let randomFortune = fortunes[fortuneIndex];
 
-        res.status(200).send(randomFortune);
-    }
-}
+    res.status(200).send(randomFortune);
+  },
+};
